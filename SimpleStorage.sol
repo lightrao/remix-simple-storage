573a46e1bee3400442beb4a162d93fde59b23352
lightrao <lightrao@hotmail.com>
2024-01-23 07:54:39 +0800

    default value is 0

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,11 +2,6 @@
 pragma solidity 0.8.18; // solidity versions
 
 contract SimpleStorage {
-    // Basic Types: boolean, uint, int, address, bytes
-    bool hasFavoriteNumber = true;
-    uint256 favoriteNumber = 88;
-    string favoriteNumberInText = "eighty-eight";
-    int256 favoriteInt = 88;
-    address myAddress = 0x3395898EEE003c9D93e4B4e7E8aF4E141A3C4885;
-    bytes32 favoriteBytes32 = "cats"; // bytes and bytes32 are diffrent
+    // favoriteNumber gets initialized to 0 if no value is given
+    uint256 favoriteNumber; // 0
 }
