8ca36392c8b77a71d446f0f80524ef4386cafaf8
lightrao <lightrao@hotmail.com>
2024-01-23 22:02:07 +0800

    view & pure

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,9 +3,17 @@ pragma solidity 0.8.18; // solidity versions
 
 contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value is given
-    uint256 public favoriteNumber;
+    uint256 favoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
+    }
+
+    function retrieve() public view returns (uint256) {
+        return favoriteNumber;
+    }
+
+    function retrieve2() public pure returns (uint) {
+        return 7;
     }
 }
