bafd6b540ff3d48cc9fcffa50bc364f14dfaf9cb
lightrao <lightrao@hotmail.com>
2024-01-23 08:12:45 +0800

    add function to contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,5 +3,9 @@ pragma solidity 0.8.18; // solidity versions
 
 contract SimpleStorage {
     // favoriteNumber gets initialized to 0 if no value is given
-    uint256 favoriteNumber; // 0
+    uint256 public favoriteNumber;
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 }
